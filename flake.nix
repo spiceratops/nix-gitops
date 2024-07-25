7355d6aeff65ba006b4ee5c664187e56da2b8542
spiceratops <stewart@spiceratops.com>
2024-07-25 13:56:55 +1000

    remove passed attribs

--- flake.nix
+++ flake.nix
@@ -26,12 +26,7 @@
       "aarch64-linux"
       "i686-linux"
       "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
     ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
