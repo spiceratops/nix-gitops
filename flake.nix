09fbd75a5cae7d8bb2433e64be329752bc494908
spiceratops <stewart@spiceratops.com>
2024-07-25 17:09:07 +1000

    test

--- flake.nix
+++ flake.nix
@@ -24,9 +24,11 @@
     # Supported systems for your flake packages, shell, etc.
     systems = [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
     ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
