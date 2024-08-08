c30ee67b12c4bfe0e0c859ff5e10087355656744
spiceratops <stewart@spiceratops.com>
2024-08-08 20:43:56 +1000

    test

--- nixos/modules/nixos/services/restic/default.nix
+++ nixos/modules/nixos/services/restic/default.nix
@@ -48,14 +48,14 @@ in
     sops.secrets = mkIf (cfg.local.enable || cfg.remote.enable) {
       "services/restic/password" = {
         sopsFile = ./secrets.sops.yaml;
-        owner = "kah";
-        group = "kah";
+        owner = 568;
+        group = 568;
       };
 
       "services/restic/env" = {
         sopsFile = ./secrets.sops.yaml;
-        owner = "kah";
-        group = "kah";
+        owner = 568;
+        group = 568;
       };
     };
 
