df538fb0da2e88478937927c7ee34c038766ccf8
spiceratops <stewart@spiceratops.com>
2025-04-15 16:09:56 +1000

    add subnet smb

--- nixos/hosts/mnas/default.nix
+++ nixos/hosts/mnas/default.nix
@@ -110,7 +110,7 @@
         #use sendfile = yes
         #max protocol = smb2
         # note: localhost is the ipv6 localhost ::1
-        hosts allow = 192.168.10. 192.168.1. 127.0.0.1 localhost
+        hosts allow = 192.168.10. 192.168.20. 192.168.1. 127.0.0.1 localhost
         hosts deny = 0.0.0.0/0
         guest account = nobody
         map to guest = bad user
